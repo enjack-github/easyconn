81b51d4bc9742734b23281b24c8c602eb81cddae
enjack-github <457003530@qq.com>
2023-06-23 16:37:38 +0800

    Update luodi.sh

--- luodi/luodi.sh
+++ luodi/luodi.sh
@@ -5,7 +5,6 @@
 #搭配中转机使用
 
 echo "下载v2ray安装脚本"
-#bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh) --remove
 rm v2ray-install-release.sh
 wget -O v2ray-install-release.sh https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh
 chmod 777 v2ray-install-release.sh
