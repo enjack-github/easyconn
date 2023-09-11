433495cae68452c5e56b1becf492832e93b215e5
enjack-github <457003530@qq.com>
2023-09-11 10:19:16 +0800

    Update checksh.sh

--- luodi/checksh.sh
+++ luodi/checksh.sh
@@ -9,21 +9,42 @@ function print_result_info() {
 	echo '当前使用的脚本是：'
 	echo $1
 	echo "------------------------------------------------------------------------------------------------"
-	echo -e "\n\n"
+	#echo -e "\n\n"
 }
 
 
 function check_what_sh_and_print() {
+	
 	pro=$(cat /usr/local/etc/v2ray/config.json)
 	if [[ $pro =~ "my_name_is:luodi6" ]];then
-	    print_result_info "wget -O luodi6.sh https://raw.githubusercontent.com/enjack-github/easyconn/main/luodi/luodi6.sh && sudo bash luodi6.sh"
+		cmdstr='wget -O luodi6.sh https://raw.githubusercontent.com/enjack-github/easyconn/main/luodi/luodi6.sh && sudo bash luodi6.sh'
+		print_result_info "$cmdstr"	     
 	elif [[ $pro =~ "my_name_is:luodi5" ]];then
-		print_result_info "wget -O luodi5.sh https://raw.githubusercontent.com/enjack-github/easyconn/main/luodi/luodi5.sh && sudo bash luodi5.sh"
+		cmdstr='wget -O luodi5.sh https://raw.githubusercontent.com/enjack-github/easyconn/main/luodi/luodi5.sh && sudo bash luodi5.sh'
+		print_result_info "$cmdstr"		
 	elif [[ $pro =~ "my_name_is:luodi3" ]];then
-		print_result_info "wget -O luodi3.sh https://raw.githubusercontent.com/enjack-github/easyconn/main/luodi/luodi3.sh && sudo bash luodi3.sh"
+		cmdstr='wget -O luodi3.sh https://raw.githubusercontent.com/enjack-github/easyconn/main/luodi/luodi3.sh && sudo bash luodi3.sh'
+		print_result_info "$cmdstr"
 	else
 	    print_result_info "unknown"
+	    exit 1
 	fi
+
+
+	#脚本功能选择
+	while true
+	do
+	read -r -p "输入【luodi】执行以上脚本，其他退出: " input
+	case $input in
+	    luodi) 
+	    		eval $cmdstr
+	    		exit 1
+	    		;;  		
+	    *) 
+	    		exit 1
+	    		;;
+	esac
+	done	
 }
 
 check_what_sh_and_print
