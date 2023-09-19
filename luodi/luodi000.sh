05af621a5adc45f111ee3e9b406d7294c28ef6d2
enjack-github <457003530@qq.com>
2023-09-19 11:21:57 +0800

    Update luodi000.sh

--- luodi/luodi000.sh
+++ luodi/luodi000.sh
@@ -764,7 +764,6 @@ function main_menu() {
 	echo -e "\e[35;46m                                   合集                                    \e[0m"
 	echo -e "\e[33;46m                                                                           \e[0m"
 	
-	rm tools.sh
 	cp luodi000.sh tools.sh
 	chmod 777 tools.sh
 	
